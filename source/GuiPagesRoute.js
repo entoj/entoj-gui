5394cdf662725ca800bf0d3ce35b8a22b3e715d3
Christian Auth <c.auth@syzygy.de>
2017-01-20 14:02:07 +0100

    Initial Checkin

---
+++ source/GuiPagesRoute.js
@@ -0,0 +1,100 @@
+'use strict';
+
+/**
+ * Requirements
+ * @ignore
+ */
+const PagesRoute = require('entoj-core/source/server/routes/PagesRoute.js').PagesRoute;
+const UrlsConfiguration = require('entoj-core/source/model/configuration/UrlsConfiguration.js').UrlsConfiguration;
+const GlobalConfiguration = require('entoj-core/source/model/configuration/GlobalConfiguration.js').GlobalConfiguration;
+const PathesConfiguration = require('entoj-core/source/model/configuration/PathesConfiguration.js').PathesConfiguration;
+const BuildConfiguration = require('entoj-core/source/model/configuration/BuildConfiguration.js').BuildConfiguration;
+const EntitiesRepository = require('entoj-core/source/model/entity/EntitiesRepository.js').EntitiesRepository;
+const EntityCategoriesRepository = require('entoj-core/source/model/entity/EntityCategoriesRepository.js').EntityCategoriesRepository;
+const SitesRepository = require('entoj-core/source/model/site/SitesRepository.js').SitesRepository;
+const CliLogger = require('entoj-core/source/cli/CliLogger.js').CliLogger;
+const Environment = require('entoj-core/source/nunjucks/Environment.js').Environment;
+const path = require('path');
+
+
+/**
+ * @memberOf server.routes
+ */
+class GuiPagesRoute extends PagesRoute
+{
+    /**
+     * @param {cli.CliLogger} cliLogger
+     * @param {model.site.SitesRepository} sitesRepository
+     * @param {model.entity.EntityCategoriesRepository} entityCategoriesRepository
+     * @param {model.entity.EntitiesRepository} entitiesRepository
+     * @param {model.configuration.GlobalConfiguration} globalConfiguration
+     * @param {model.configuration.UrlsConfiguration} urlsConfiguration
+     * @param {model.configuration.PathesConfiguration} pathesConfiguration
+     * @param {model.configuration.BuildConfiguration} buildConfiguration
+     * @param {nunjucks.Environment} nunjucks
+     */
+    constructor(cliLogger, sitesRepository, entityCategoriesRepository, entitiesRepository, globalConfiguration,
+                urlsConfiguration, pathesConfiguration, buildConfiguration, nunjucks)
+    {
+        const routes =
+        [
+            {
+                url: '/',
+                template: 'sites.j2'
+            },
+            {
+                url: '/:site',
+                template: 'entityCategories.j2'
+            },
+            {
+                url: '/:site/:entityCategory',
+                template: 'entityCategory.j2'
+            },
+            {
+                url: '/:site/:entityCategory/:entityId',
+                template: 'entity.j2'
+            },
+            {
+                url: '/:site/:entityCategory/:entityId/examples',
+                template: 'entityExamples.j2'
+            },
+            {
+                url: '/:site/:entityCategory/:entityId/documentation',
+                template: 'entityDocumentation.j2'
+            }
+        ];
+        const options =
+        {
+            templateRoot: path.resolve(__dirname + '/template'),
+            staticRoute: '/_'
+        };
+        super(cliLogger.createPrefixed('gui'), sitesRepository, entityCategoriesRepository, entitiesRepository, globalConfiguration,
+                urlsConfiguration, pathesConfiguration, buildConfiguration, nunjucks, routes, options);
+    }
+
+
+    /**
+     * @inheritDoc
+     */
+    static get injections()
+    {
+        return { 'parameters': [CliLogger, SitesRepository, EntityCategoriesRepository, EntitiesRepository,
+                                GlobalConfiguration, UrlsConfiguration, PathesConfiguration, BuildConfiguration, Environment] };
+    }
+
+
+    /**
+     * @inheritDocs
+     */
+    static get className()
+    {
+        return 'server.routes/GuiPagesRoute';
+    }
+}
+
+
+/**
+ * Exports
+ * @ignore
+ */
+module.exports.GuiPagesRoute = GuiPagesRoute;
---
+++ source/template/js/entity.js
@@ -0,0 +1,96 @@
+(function()
+{
+    // Update entities to reflect search settings
+    function updateSearch()
+    {
+        // Prepare
+        var states = { concept:[], frontend:[], integration:[] };
+        $('.content-navigation .state.popup .item').each(function()
+        {
+            var $item = $(this);
+            if ($item.hasClass('selected'))
+            {
+                var $items = $item.closest('.list');
+                var type = 'concept';
+                if ($items.hasClass('frontend'))
+                {
+                    type = 'frontend';
+                }
+                if ($items.hasClass('integration'))
+                {
+                    type = 'integration';
+                }
+                states[type].push($item.text().toLowerCase());
+            }
+        });
+        var search = $('.content-navigation .search.input input').val();
+        var matcher = new RegExp(search, 'i');
+
+        // Search entities
+        $('.entity').each(function()
+        {
+            $entity = $(this);
+            var textMatches = false;
+
+            // Text matches
+            if (matcher.test($('.header', $entity).text()))
+            {
+                textMatches = true;
+            }
+            if (!textMatches && matcher.test($('.description', $entity).text()))
+            {
+                textMatches = true;
+            }
+
+            // State matches
+            var stateMatches = (states.concept.indexOf($entity.find('.state.concept .value').text().toLowerCase()) !== -1) &&
+                (states.frontend.indexOf($entity.find('.state.frontend .value').text().toLowerCase()) !== -1) &&
+                (states.integration.indexOf($entity.find('.state.integration .value').text().toLowerCase()) !== -1);
+
+            // Show / Hide entities
+            if (textMatches && stateMatches)
+            {
+                $entity.show();
+            }
+            else
+            {
+                $entity.hide();
+            }
+        });
+
+        // Show / Hide categories
+        $('.entitycategory').each(function()
+        {
+            $entityCategory = $(this);
+            $entityCategory.show();
+            if ($('.entity:visible', $entityCategory).length === 0)
+            {
+                $entityCategory.hide();
+            }
+        });
+    }
+
+
+    // States
+    $('.content-navigation .browse.item').popup(
+    {
+        on: 'click',
+        popup: '.content-navigation .state.popup',
+        position: 'top center',
+        inline: true,
+        distanceAway: -8
+    });
+    $('.content-navigation .state.popup .item').on('click', function()
+    {
+        var $item = $(this);
+        $item.toggleClass('selected');
+        updateSearch();
+    });
+
+
+    // Search
+    $('.content-navigation .search.input').on('input', function(e)
+    {
+        updateSearch();
+    });
+})();
